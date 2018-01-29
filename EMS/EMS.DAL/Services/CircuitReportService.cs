cc20b9fe112649df894d75e279f28e8fb3fa27e8
Fight4benben <zhangbenclass3@126.com>
2018-01-29 10:17:44 +0800

    在CircuitReportSercie中增加注释

--- EMS/EMS.DAL/Services/CircuitReportService.cs
+++ EMS/EMS.DAL/Services/CircuitReportService.cs
@@ -26,7 +26,7 @@ namespace EMS.DAL.Services
         /// 初始加载：获取用户名查询建筑列表，第一栋建筑对应的分类，第一个分类对应的回路，所有回路的日报表（时间是当日）
         /// </summary>
         /// <param name="userName">用户名</param>
-        /// <returns></returns>
+        /// <returns>返回完整的数据：包含建筑列表，能源按钮列表，树状结构，数据</returns>
         public CircuitReportViewModel GetViewModel(string userName)
         {
             DateTime today = DateTime.Now;
@@ -54,7 +54,14 @@ namespace EMS.DAL.Services
 
             return circuitReportView;
         }
-
+    
+        /// <summary>
+        /// 修改建筑名称返回对应数据
+        /// </summary>
+        /// <param name="buildId">传入建筑ID</param>
+        /// <param name="type">传入报表类型</param>
+        /// <param name="date">传入日期</param>
+        /// <returns>返回信息不包含建筑列表（建筑列表已经填充）</returns>
         public CircuitReportViewModel GetViewModel(string buildId,string type,string date)
         {
             if (type == "MM")
@@ -86,6 +93,14 @@ namespace EMS.DAL.Services
             return circuitReportView;
         }
 
+        /// <summary>
+        /// 建筑ID与能源类型，返回数据：建筑列表与能源分类按钮已经存在，不需要再次回传数据
+        /// </summary>
+        /// <param name="buildId">建筑ID</param>
+        /// <param name="energyCode">能源类型</param>
+        /// <param name="type">报表类型</param>
+        /// <param name="date">日期</param>
+        /// <returns>前端已有建筑列表与能源按钮列表，无需再次上传这些数据，只要上传树状结构数据，和报表数据</returns>
         public CircuitReportViewModel GetViewModel(string buildId,string energyCode, string type, string date)
         {
             if (type == "MM")
@@ -114,6 +129,15 @@ namespace EMS.DAL.Services
             return circuitReportView;
         }
 
+        /// <summary>
+        /// 传入建筑ID，能源类型，选中的回路编号
+        /// </summary>
+        /// <param name="buildId">建筑ID</param>
+        /// <param name="energyCode">能源类型</param>
+        /// <param name="circuits">一个或多个回路Id</param>
+        /// <param name="type">报表类型</param>
+        /// <param name="date">日期</param>
+        /// <returns>只返回数据即可</returns>
         public CircuitReportViewModel GetViewModel(string buildId, string energyCode, string circuits,string type, string date)
         {
             if (type == "MM")
@@ -141,7 +165,12 @@ namespace EMS.DAL.Services
         //{
         //    DateTime today = Utils.Util.ConvertString2DateTime(date,"yyyy-MM-dd");
         //}
-
+        /// <summary>
+        /// 根据建筑ID，和能源类型获取树状结构
+        /// </summary>
+        /// <param name="buildId">建筑ID</param>
+        /// <param name="energyItemCode">能源类型</param>
+        /// <returns>树状结构</returns>
         public List<TreeViewModel> GetTreeListViewModel(string buildId, string energyItemCode)
         {
             List<Circuit> circuits = context.GetCircuitListByBIdAndEItemCode(buildId,energyItemCode);
@@ -161,7 +190,13 @@ namespace EMS.DAL.Services
 
             return treeList;
         }
-
+        
+        /// <summary>
+        /// 递归调用方式填充树状结构的子节点
+        /// </summary>
+        /// <param name="circuits"></param>
+        /// <param name="circuit"></param>
+        /// <returns></returns>
         List<TreeViewModel> GetChildrenNodes(List<Circuit> circuits, Circuit circuit)
         {
             string parentId = circuit.CircuitId;
@@ -193,6 +228,16 @@ namespace EMS.DAL.Services
             return list.ToArray();
         }
 
+        /// <summary>
+        /// 根据传入参数，将数据导出到Excel中（日报，月报，年报）
+        /// </summary>
+        /// <param name="basePath">App_Data</param>
+        /// <param name="buildId"></param>
+        /// <param name="energyCode"></param>
+        /// <param name="circuits"></param>
+        /// <param name="type"></param>
+        /// <param name="date"></param>
+        /// <returns>Excel实体类，包含Excel文件的byte数组与Excel文件的名称</returns>
         public Excel ExportCircuitReportToExcel(string basePath,string buildId, string energyCode, string circuits, string type, string date)
         {
             string templatePath=basePath +"/DayReportTemplate.xls";
@@ -220,6 +265,7 @@ namespace EMS.DAL.Services
             HSSFWorkbook book = new HSSFWorkbook(file);
             HSSFSheet sheet = (HSSFSheet)book.GetSheet("Sheet1");
 
+            //创建单元格格式：数字型单元格默认取小数点后两位
             ICellStyle style = book.CreateCellStyle();
             style.DataFormat = HSSFDataFormat.GetBuiltinFormat("0.00");
 
@@ -227,6 +273,8 @@ namespace EMS.DAL.Services
             //获取当前建筑名
             IHomeDbContext homeContext = new HomeDbContext();
             BuildInfo build = homeContext.GetBuildById(buildId);
+
+            //获取能源类型
             EnergyItemDict energyItem = context.GetUnitByEnergyCode(energyCode);
 
             DateTime now = Utils.Util.ConvertString2DateTime(date, "yyyy-MM-dd");
@@ -247,6 +295,7 @@ namespace EMS.DAL.Services
             int rowId = 0;
             for (int i = 0; i < circuitIds.Length; i++)
             {
+                //使用lamda表达式筛选List中Id与传入的Id对应的仪表：一次填充一行Excel
                 List<ReportValue> current = data.FindAll(p=>p.Id==circuitIds[i]);
                 if (current.Count > 0)
                 {
@@ -257,7 +306,8 @@ namespace EMS.DAL.Services
                     {
                         case "DD":
                             foreach (var item in current)
-                            {
+                            {//遍历筛选出列表中当前回路中每个小时的数据，填充到Excel中
+                                //当前行的第一列已经填充回路名称，根据时间设置向右偏移一位，将对应数据写入到当前单元格，并设置格式
                                 row.CreateCell(item.Time.Hour + 1).SetCellValue((double)item.Value);
                                 row.GetCell(item.Time.Hour+1).CellStyle = style;
                                 total += item.Value;
