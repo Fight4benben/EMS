989f998aec74ff8f3b9bfcaf26904cb4c4ddb9be
张奔 <zhangbenclass3@126.com>
2018-07-16 10:44:30 +0800

    区域用能概况页面完全完成

--- EMS/EMS.UI/app/js/Region/Main.js
+++ EMS/EMS.UI/app/js/Region/Main.js
@@ -11,10 +11,22 @@ var RegionMain = (function(){
 		this.initDom = function(){
 			$("#energylist").change(function(event) {
 				var url = "/api/RegionMain";
+				var filter = energys.filter(function(item){
+					return item.energyItemCode == $("#energylist").val();
+				});
+
+				if(filter.length>0)
+					unit = filter[0].energyItemUnit;
+				else 
+					unit = "";
 				getDataFromServer(url,"buildId="+$("#buildinglist").val()+
-					"&energyCode="+$("#energylist").val())
+					"&energyCode="+$("#energylist").val());
 			});
 		}
+
+		var rankData=[];
+		var energys;
+		var unit;
 
 		function getDataFromServer(url,params){
 			EMS.Loading.show($("#main-content").parent('div'));
@@ -23,13 +35,13 @@ var RegionMain = (function(){
 				if(data.hasOwnProperty('message'))
 					location = "/Account/Login";
 				try{
-					console.log(data);
+					//console.log(data);
 					showBuildList(data);
 					showEnergys(data);
-					showCompare(data)
-					//showRank(data);
-					//showPie(data);
-					//showStackBar(data);
+					showCompare(data);
+					showRank(data);
+					showPie(data);
+					showStackBar(data);
 				}catch(e){
 
 				}finally{
@@ -60,6 +72,11 @@ var RegionMain = (function(){
 		function showEnergys(data){
 			if(!data.hasOwnProperty('energys'))
 				return;
+
+			energys = data.energys;
+
+			unit = energys[0].energyItemUnit;
+
 			EMS.DOM.initSelect(data.energys,$("#energylist"),"energyItemName","energyItemCode");
 		}
 
@@ -124,14 +141,145 @@ var RegionMain = (function(){
 			var ids = [];
 
 			$.each(data.rankValues, function(key, val) {
-				if(!$.inArray(val.classifyID, ids)){
+				if($.inArray(val.classifyID, ids)<0){
 					ids.push(val.classifyID);
 					$("#regionBtns").append('<div class="col-sm-12 col-xs-3" value="'+
 						val.classifyID+'"><span>'+val.classifyName+'</span></div>');
+
+					var regionFilter = data.rankValues.filter(function(item) {
+						return item.classifyID == val.classifyID;
+					});
+
+					var obj = {name:[],data:[]};
+					$.each(regionFilter, function(index, value) {
+						obj.name[index] = value.name;
+						obj.data[index] = value.value;
+					});
+
+					rankData[val.classifyID] = obj;
 				}
 			});
 
+			if(ids.length ==0)
+				return;
 
+			$("#regionBtns>div").eq(0).addClass('le-time-select');
+
+			drawRank($(".le-time-select").attr('value'));
+
+			$("#regionBtns>div").click(function(event) {
+				var $this = $(this);
+				var code = $this.attr('value');
+
+				if($this.hasClass('le-time-select'))
+					return;
+
+				$(".le-time-select").removeClass('le-time-select');
+
+				$this.addClass('le-time-select');
+
+				drawRank(code);
+			});
+		}
+
+		function drawRank(code){
+
+			var name = ["", "", "", "", "", "", "", "", "", ""];
+			var value =[0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+
+			$.each(rankData[code].name, function(key, val) {
+				name[key] = val;
+			});
+
+			$.each(rankData[code].data, function(key, val) {
+				value[key] = val;
+			});
+
+			var series = {
+            	type: 'bar',
+            	data:value
+			}
+			EMS.Chart.showBar(echarts,$("#rankBar"),undefined,name,series);
+		}
+
+		//显示饼图
+		function showPie(data){
+
+			$("#monthPie").html("");
+
+			if(!data.hasOwnProperty("pieValues"))
+				return;
+
+			if(data.pieValues.length <=0)
+				return;
+
+			var names=[];
+			var values=[];
+			$.each(data.pieValues, function(key, val) {
+				values.push({name:val.name,value:val.value});
+				names.push(val.name);
+			});
+
+			EMS.Chart.showPie(echarts,$('#monthPie'),names,values,"区域用能");
+		}
+
+		function showStackBar(data){
+			$("#monthStackBar").html("");
+
+			if(!data.hasOwnProperty('stackValues'))
+				return;
+
+			if(data.stackValues.length<=0)
+				return;
+
+			var curDate = new Date();
+			curDate.setDate(curDate.getDate()-31);
+			var startDate = new Date(curDate.getFullYear(),curDate.getMonth(),curDate.getDate());
+			var times=[];
+			var dataArray = [];
+			var names=[];
+
+			for (var i=0; i<=31; i++) {
+
+				startDate.setDate(startDate.getDate()+i);
+				var tempDate = new Date(startDate.getFullYear(),startDate.getMonth(),startDate.getDate(),0,0,0);
+
+				times.push((tempDate.getMonth()+1)+"-"+(tempDate.getDate()));
+
+				var array = data.stackValues.filter(function(item){
+					var inDate = new Date(item.time);
+					return (tempDate.getFullYear() == inDate.getFullYear()) && 
+						(tempDate.getMonth() == inDate.getMonth())
+					&& (tempDate.getDate() == inDate.getDate());
+				});
+
+				//console.log(array);
+				if(array.length >0 ){
+					$.each(array, function(key, val) {
+
+						var outArray = dataArray.filter(function(itemData,index) {
+							return itemData.name == val.name; 
+						});
+
+						if(outArray.length <= 0){
+							dataArray.push({name:val.name,type:'bar',stack:'itemStack',data:[val.value.toFixed(1)]});
+						}else{
+							$.each(dataArray, function(index, value) {
+								if(value.name == val.name)
+									value.data.push(val.value.toFixed(1));
+							});
+						}
+					});
+				}
+
+				startDate.setDate(startDate.getDate()-i);
+			}
+
+			$.each(dataArray, function(index, val) {
+				names.push(val.name);
+			});
+
+			EMS.Chart.showStackBar(echarts,$("#monthStackBar"),names,times,dataArray,unit);
 		}
 
 	};
