82411de5a56d46ce6b1c4328fe95277040c007be
xiaoshi WEN <love2fuji@outlook.com>
2018-07-17 16:20:40 +0800

    添加 区域用能同比分析完成

---
+++ EMS/EMS.DAL/IRepository/IRegionCompareDbContext.cs
@@ -0,0 +1,15 @@
+﻿using EMS.DAL.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EMS.DAL.IRepository
+{
+    public interface IRegionCompareDbContext
+    {
+        List<EMSValue> GetCompareValueList(string energyCode, string regionID, string date);
+        List<TreeViewInfo> GetTreeViewInfoList(string buildId, string energyCode);
+    }
+}
---
+++ EMS/EMS.Tests/Services/RegionCompareServiceTest.cs
@@ -0,0 +1,44 @@
+﻿using EMS.DAL.Services;
+using EMS.DAL.ViewModels;
+using EMS.Tests.Utils;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EMS.Tests.Services
+{
+    [TestClass]
+    public class RegionCompareServiceTest
+    {
+        RegionCompareService service = new RegionCompareService();
+
+        [TestMethod]
+        public void TestGetRegionCompareViewModelByUser()
+        {
+            RegionCompareViewModel compareViewModel = service.GetViewModelByUserName("admin");
+
+            Console.WriteLine(UtilTest.GetJson(compareViewModel));
+        }
+
+        [TestMethod]
+        public void TestGetRegionCompareViewModelByBuild_EnergyCode()
+        {
+            DateTime today = DateTime.Now;
+            RegionCompareViewModel reportViewModel = service.GetViewModel("000001G001", "01000");
+
+            Console.WriteLine(UtilTest.GetJson(reportViewModel));
+        }
+
+        [TestMethod]
+        public void TestGetRegionCompareViewModelByRegionID_EnergyCode_Date()
+        {
+            DateTime today = DateTime.Now;
+            RegionCompareViewModel compareViewModel = service.GetViewModel("01000", "000001G0010002", today.ToString());
+
+            Console.WriteLine(UtilTest.GetJson(compareViewModel));
+        }
+    }
+}
