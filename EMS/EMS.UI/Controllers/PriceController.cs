9c054d258d3b2379860524b1c7f313003646a428
xiaoshi WEN <love2fuji@outlook.com>
2018-08-01 15:27:56 +0800

    添加 PriceController

---
+++ EMS/EMS.UI/Controllers/PriceController.cs
@@ -0,0 +1,66 @@
+﻿using EMS.DAL.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace EMS.UI.Controllers
+{
+    [Authorize]
+    public class PriceController : ApiController
+    {
+        PriceService service = new PriceService();
+
+        public object Get()
+        {
+            try
+            {
+                string userName = User.Identity.Name;
+                return service.GetViewModelByUserName(userName);
+            }
+            catch (Exception e)
+            {
+                return e.Message;
+            }
+        }
+
+        public object Get(string buildId, string date, string type)
+        {
+            try
+            {
+                return service.GetViewModel(buildId, date, type);
+            }
+            catch (Exception e)
+            {
+                return e.Message;
+            }
+        }
+
+        public object Get(string buildId, string energyCode, string date, string type)
+        {
+            try
+            {
+                return service.GetViewModel(buildId, energyCode, date, type);
+            }
+            catch (Exception e)
+            {
+                return e.Message;
+            }
+        }
+
+        public object Get(string buildId, string energyCode, string regionIDs, string date, string type)
+        {
+            try
+            {
+                string[] ids = regionIDs.Split(',');
+                return service.GetViewModel(buildId, energyCode, ids, date, type);
+            }
+            catch (Exception e)
+            {
+                return e.Message;
+            }
+        }
+    }
+}
