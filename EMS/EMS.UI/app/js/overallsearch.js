e41f6c940eb2414aab5e70fcf9df6d7130e1a1eb
张奔 <zhangbenclass3@126.com>
2018-10-08 15:12:46 +0800

    综合查询新增功能已经完成

--- EMS/EMS.UI/app/js/overallsearch.js
+++ EMS/EMS.UI/app/js/overallsearch.js
@@ -4,6 +4,7 @@ var OverAll = (function(){
 
 		this.show = function(){
 			initBuilds();
+			initDom();
 			$(".searchBtn").click(function(event) {
 				var id = $(this)[0].id;
 				var keyWord = $("#search-keyword").val();
@@ -13,10 +14,69 @@ var OverAll = (function(){
 					return;
 				}
 
+				var date; 
+
+				var type =$("#dateType").val();
+				if(type=="QQ"){
+					var season = $("#season").val();
+					date = $("#daycalendarBox").val()+"-"+EMS.Tool.appendZero(parseInt(season));
+				}else
+					date = $("#daycalendarBox").val();
+
 				var url="/api/OverAllSearch";
-				var params ="type="+id+"&keyWord="+keyWord+"&buildID="+$("#buildinglist").val()+"&endDay="+EMS.Tool.dateFormat(new Date());
+				var params ="timeType="+type+"&type="+id+"&keyWord="+keyWord+"&buildID="+$("#buildinglist").val()+"&energyCode=01000"+"&endDay="+date;
 
 				getDataFromServer(url,params,id);
+			});
+		}
+
+		function initDom(){
+			//季度选择框默认不显示
+			showOrHideSeason(false);
+			//初始化日期
+			initDate("DD");
+
+			initDateType();
+		}
+
+		function showOrHideSeason(isShow){
+			isShow?$(".season-alarm").show():$(".season-alarm").hide();
+		}
+
+		function initDate(type){
+			switch(type){
+				case "DD"://日
+				EMS.DOM.initDateTimePicker('CURRENTDATE',new Date(),$("#dayCalendar"),$("#daycalendarBox"));
+				break;
+				case "MM"://月
+				EMS.DOM.initDateTimePicker('YEARMONTH',new Date(),$("#dayCalendar"),$("#daycalendarBox"),{format:'yyyy-mm',
+									        language: 'zh-CN',
+									        autoclose: 1,
+									        startView: 3,
+									        minView: 3,
+									        forceParse: false,
+									        pickerPosition: "bottom-left"});
+				break;
+				case "QQ"://季度
+				EMS.DOM.initDateTimePicker('YEAR',new Date(),$("#dayCalendar"),$("#daycalendarBox"),{format:'yyyy',
+									        language: 'zh-CN',
+									        autoclose: 1,
+									        startView: 4,
+									        minView: 4,
+									        forceParse: false,
+									        pickerPosition: "bottom-left"});
+				break;
+
+			}
+		}
+
+		function initDateType(){
+			$("#dateType").change(function(event) {
+				var typeValue = $(this).val();
+				initDate(typeValue);
+
+				typeValue === "QQ" ? showOrHideSeason(true) : showOrHideSeason(false);
+
 			});
 		}
 
@@ -41,13 +101,14 @@ var OverAll = (function(){
 		function getDataFromServer(url,params,type){
 			EMS.Loading.show();
 			$.getJSON(url,params, function(data) {
-				//console.log(data);
+
 				try{
 					showTrendData(data);
 					showTable(data,type);
 					showRingRatio(data);
+					showEnergyAvgs(data);
 				}catch(exception){
-
+					console.log(exception);
 				}finally{
 					EMS.Loading.hide();
 				}
@@ -63,8 +124,25 @@ var OverAll = (function(){
 			var times=[];
 			var values=[];
 
-			$.each(data.last31Day, function(index, val) {
-				times.push(val.time.split(' ')[0]);
+			var position;
+
+			var typeValue = $("#dateType").val();
+
+			switch(typeValue){
+				case "DD"://日
+					position = 1;
+				break;
+				case "MM"://月
+					position = 0
+				break;
+				case "QQ"://季度
+					position = 0;
+				break;
+
+			}
+
+			$.each(data.timeData, function(index, val) {
+				times.push(val.time.split(' ')[position]);
 				values.push(val.value);
 			});
 
@@ -101,15 +179,34 @@ var OverAll = (function(){
 				break;
 			}
 
-			for (var i = 1; i <= currentDate.getDate(); i++) {
-				columns.push({field:i,title:i+'日'});
+			var typeValue = $("#dateType").val();
+
+			switch(typeValue){
+				case "DD"://日
+					for (var i = 0; i <= 23; i++) {
+						columns.push({field:i,title:i+"时"});
+					}
+				break;
+				case "MM"://月
+					var monthDate = new Date(parseInt($("#daycalendarBox").val().split('-')[0]),
+						parseInt($("#daycalendarBox").val().split('-')[1]),0).getDate();
+					for (var i = 1; i <= monthDate; i++) {
+						columns.push({field:i,title:i+"日"});
+					}
+				break;
+				case "QQ"://季度
+					var endMonth = $("#season").val();
+					for (var i = endMonth-2; i <= endMonth; i++) {
+						columns.push({field:i,title:i+"月"});
+					}
+				break;
 			}
 
 			var row={};
-			$.each(data.monthDate, function(key, val) {
+			$.each(data.timeData, function(key, val) {
 				var time = new Date(val.time);
 				row.name = val.name;
-				row[time.getDate()] = val.value;
+				row[getColumnByType(typeValue,time)] = val.value;
 			});
 
 			rows.push(row);
@@ -118,6 +215,24 @@ var OverAll = (function(){
 			$("#dayReport>table").attr('data-height',$("#dayReport").height());
 
 			EMS.DOM.showTable($("#dayReport>table"),columns,rows,{striped:true,classes:'table table-border'});
+		}
+
+		function getColumnByType(type,time){
+			var timeIndex;
+
+			switch(type){
+				case "DD"://日
+					timeIndex = time.getHours();
+				break;
+				case "MM"://月
+					timeIndex = time.getDate();
+				break;
+				case "QQ"://季度
+					timeIndex = time.getMonth()+1;
+				break;
+			}
+
+			return timeIndex;
 		}
 
 		function showRingRatio(data){
@@ -144,6 +259,32 @@ var OverAll = (function(){
 			}
 			else
 				$("#ringRatio").html("-");
+		}
+
+		function showEnergyAvgs(data){
+			if(data.hasOwnProperty('monthAverageData')){
+				if(data.monthAverageData.length > 0){
+					data.monthAverageData[0].hasOwnProperty('areaAvg')
+						? $("#square-month>h3").html(data.monthAverageData[0].areaAvg+"kW·h/㎡") 
+						: $("#square-month>h3").html("-");
+
+					data.monthAverageData[0].hasOwnProperty('peopleAvg') 
+						? $("#people-month>h3").html(data.monthAverageData[0].peopleAvg+"kW·h/人") 
+						: $("#people-month>h3").html("-");	
+				}
+			}
+
+			if(data.hasOwnProperty('yearAverageData')){
+				if(data.yearAverageData.length > 0){
+					data.yearAverageData[0].hasOwnProperty('areaAvg')
+						? $("#square-year>h3").html(data.yearAverageData[0].areaAvg+"kW·h/㎡") 
+						: $("#square-year>h3").html("-");
+
+					data.yearAverageData[0].hasOwnProperty('peopleAvg') 
+						? $("#people-year>h3").html(data.yearAverageData[0].peopleAvg+"kW·h/人") 
+						: $("#people-year>h3").html("-");	
+				}
+			}
 		}
 	};
 
