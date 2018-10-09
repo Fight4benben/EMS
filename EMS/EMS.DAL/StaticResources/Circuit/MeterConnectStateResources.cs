1a54378bdae7be5eeef108c69641cf766936b584
张奔 <zhangbenclass3@126.com>
2018-10-09 22:06:37 +0800

    修改仪表通讯状态，大于15分钟才会标记通讯中断

--- EMS/EMS.DAL/StaticResources/Circuit/MeterConnectStateResources.cs
+++ EMS/EMS.DAL/StaticResources/Circuit/MeterConnectStateResources.cs
@@ -15,9 +15,11 @@ namespace EMS.DAL.StaticResources
                                                 SELECT  MeterUseInfo.F_MeterID AS ID
                                                         ,F_MeterName AS Name
                                                         ,DataCollectionInfo.F_CollectionName AS CollectionName
-                                                        , CASE WHEN F_DisConnect = 0 THEN '通讯正常' ELSE '通讯中断' END AS States
-                                                        ,F_DisConnectTime  AS DisConnectTime 
-	                                                    , CONVERT( VARCHAR(3), DATEDIFF(MINUTE,F_DisConnectTime,GETDATE())/1440)+':'+LEFT(CONVERT(VARCHAR(20),DATEADD(MINUTE,DATEDIFF(MINUTE,F_DisConnectTime,GETDATE()),0),114),5) DiffDate	
+                                                        , CASE WHEN F_DisConnect = 0 OR DATEDIFF(MINUTE,F_DisConnectTime,GETDATE())<15 OR F_DisConnectTime = NULL THEN '通讯正常' ELSE '通讯中断' END AS States
+                                                        ,CASE WHEN F_DisConnectTime IS NULL  OR DATEDIFF(MINUTE,F_DisConnectTime,GETDATE())<15 THEN NULL ELSE F_DisConnectTime END  AS DisConnectTime 
+                                                        ,CASE WHEN DATEDIFF(MINUTE,F_DisConnectTime,GETDATE())<15 THEN NULL ELSE  CAST(DATEDIFF(MINUTE,F_DisConnectTime,GETDATE())/1440 as varchar(5)) 
+	                                                        +'天'+CAST( DATEDIFF(MINUTE,F_DisConnectTime,GETDATE())/60 as varchar(5)) 
+	                                                        +'时'+CAST(DATEDIFF(MINUTE,F_DisConnectTime,GETDATE())%60 as varchar(5))+'分' END DiffDate	
                                                     FROM T_ST_MeterUseInfo AS MeterUseInfo
                                                     INNER JOIN T_ST_DataCollectionInfo AS DataCollectionInfo ON DataCollectionInfo.F_CollectionID=MeterUseInfo.F_CollectionID
                                                     INNER JOIN T_ST_CircuitMeterInfo Circuit ON MeterUseInfo.F_MeterID = Circuit.F_MeterID
@@ -31,16 +33,19 @@ namespace EMS.DAL.StaticResources
         public static string MeterOfflineStateSQL = @"
                                                     SELECT  MeterUseInfo.F_MeterID AS ID
                                                             ,F_MeterName AS Name
-                                                            ,DataCollectionInfo.F_CollectionName AS CollectionName
-                                                            , CASE WHEN F_DisConnect = 0 THEN '通讯正常' ELSE '通讯中断' END AS States
-                                                            ,F_DisConnectTime  AS DisConnectTime 
-	                                                        ,CONVERT( VARCHAR(3), DATEDIFF(MINUTE,F_DisConnectTime,GETDATE())/1440)+':'+LEFT(CONVERT(VARCHAR(20),DATEADD(MINUTE,DATEDIFF(MINUTE,F_DisConnectTime,GETDATE()),0),114),5) DiffDate	
+                                                        ,DataCollectionInfo.F_CollectionName AS CollectionName
+                                                        , CASE WHEN F_DisConnect = 0 OR DATEDIFF(MINUTE,F_DisConnectTime,GETDATE())<15 OR F_DisConnectTime = NULL THEN '通讯正常' ELSE '通讯中断' END AS States
+                                                        ,CASE WHEN F_DisConnectTime IS NULL  OR DATEDIFF(MINUTE,F_DisConnectTime,GETDATE())<15 THEN NULL ELSE F_DisConnectTime END  AS DisConnectTime 
+                                                        ,CASE WHEN DATEDIFF(MINUTE,F_DisConnectTime,GETDATE())<15 THEN NULL ELSE  CAST(DATEDIFF(MINUTE,F_DisConnectTime,GETDATE())/1440 as varchar(5)) 
+	                                                        +'天'+CAST( DATEDIFF(MINUTE,F_DisConnectTime,GETDATE())/60 as varchar(5)) 
+	                                                        +'时'+CAST(DATEDIFF(MINUTE,F_DisConnectTime,GETDATE())%60 as varchar(5))+'分' END DiffDate	
                                                         FROM T_ST_MeterUseInfo AS MeterUseInfo
                                                         INNER JOIN T_ST_DataCollectionInfo AS DataCollectionInfo ON DataCollectionInfo.F_CollectionID=MeterUseInfo.F_CollectionID
                                                         INNER JOIN T_ST_CircuitMeterInfo Circuit ON MeterUseInfo.F_MeterID = Circuit.F_MeterID
                                                         where MeterUseInfo.F_BuildID=@BuildID
                                                         AND Circuit.F_EnergyItemCode=@EnergyItemCode
                                                         AND F_DisConnect=@Type
+                                                        AND DATEDIFF(MINUTE,F_DisConnectTime,GETDATE())>=15
                                                 ";
     }
 }
