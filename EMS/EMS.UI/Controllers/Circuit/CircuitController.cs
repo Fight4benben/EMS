c5a2f496bca264a315430116a9df0559b6876278
张奔 <zhangbenclass3@126.com>
2018-10-29 13:06:06 +0800

    添加参数查询路由

--- EMS/EMS.UI/Controllers/Circuit/CircuitController.cs
+++ EMS/EMS.UI/Controllers/Circuit/CircuitController.cs
@@ -37,6 +37,11 @@ namespace EMS.UI.Controllers
             return View();
         }
 
+        public ActionResult Param()
+        {
+            return View();
+        }
+
         public FileResult GetExcel(string buildId, string energyCode, string type, string circuits,string date)
         {
             string[] circuitArray = circuits.Split(',');
