99a59b96f91d33503b68f605fd6ee0782e4152b7
xiaoshi WEN <love2fuji@outlook.com>
2018-12-18 14:33:43 +0800

    添加 单位面积能耗 控制器层

---
+++ EMS/EMS.Tests/Services/DepartmentAreaAvgRankServiceTest.cs
@@ -0,0 +1,37 @@
+﻿using EMS.DAL.Services;
+using EMS.DAL.ViewModels;
+using EMS.Tests.Utils;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EMS.Tests.Services
+{
+    [TestClass]
+    public class DepartmentAreaAvgRankServiceTest
+    {
+        [TestMethod]
+        public void TestGetDepartmentAreaAvgRankViewModelByUser()
+        {
+            DateTime today = DateTime.Now;
+            DepartmentAreaAvgRankService service = new DepartmentAreaAvgRankService();
+            DepartmentAreaAvgRankViewModel ViewModel = service.GetViewModelByUserName("admin");
+
+            Console.WriteLine(UtilTest.GetJson(ViewModel));
+
+        }
+
+        [TestMethod]
+        public void TestGetDepartmentAreaAvgRankViewModelByMonth()
+        {
+            DateTime today = DateTime.Now;
+            DepartmentAreaAvgRankService service = new DepartmentAreaAvgRankService();
+            DepartmentAreaAvgRankViewModel ViewModel = service.GetViewModel("000001G003", "01000", "2018-08");
+
+            Console.WriteLine(UtilTest.GetJson(ViewModel));
+        }
+    }
+}
---
+++ EMS/EMS.UI/Controllers/Department/DepartmentAreaAvgRankController.cs
@@ -0,0 +1,47 @@
+﻿using EMS.DAL.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+
+namespace EMS.UI.Controllers.Department
+{
+    [Authorize]
+    public class DepartmentAreaAvgRankController : ApiController
+    {
+        DepartmentAreaAvgRankService service = new DepartmentAreaAvgRankService();
+
+        /// <summary>
+        /// 部门 月份 单位面积能耗
+        /// 初始加载：获取用户名查询建筑列表，第一栋建筑对应的分类，第一个分类对应的所有部门
+        /// </summary>
+        /// <param name="userName">用户名</param>
+        /// <returns>包含建筑列表，能源按钮列表，部门单位面积能耗</returns>
+        public object Get()
+        {
+            try
+            {
+                string userName = User.Identity.Name;
+                return service.GetViewModelByUserName(userName);
+            }
+            catch (Exception e)
+            {
+                return e.Message;
+            }
+        }
+
+        public object Get(string buildId, string energyCode, string date)
+        {
+            try
+            {
+                return service.GetViewModel(buildId, energyCode, date);
+            }
+            catch (Exception e)
+            {
+                return e.Message;
+            }
+        }
+
+    }
+}
