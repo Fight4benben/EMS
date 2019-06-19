8d5b3e2d733ec4df01b38dc2aeea954d76b59ec0
xiaoshi WEN <love2fuji@outlook.com>
2019-06-19 16:39:11 +0800

    添加 复费率接口注释

--- EMS/EMS.DAL/StaticResources/Circuit/MeterConnectStateResources.cs
+++ EMS/EMS.DAL/StaticResources/Circuit/MeterConnectStateResources.cs
@@ -17,7 +17,7 @@ namespace EMS.DAL.StaticResources
                                                     ,CASE WHEN  DATEDIFF(MINUTE,DataCollectionInfo.F_LastUpTime,GETDATE()) > 20 OR ( F_DisConnect = 1 OR DATEDIFF(MINUTE,F_DisConnectTime,GETDATE())>10 )THEN '通讯中断' ELSE '通讯正常' END AS States
                                                     ,CASE WHEN F_DisConnectTime IS NULL  OR DATEDIFF(MINUTE,F_DisConnectTime,GETDATE())<15 THEN NULL ELSE F_DisConnectTime END  AS DisConnectTime 
                                                     ,CASE WHEN DATEDIFF(MINUTE,F_DisConnectTime,GETDATE())<15 THEN NULL ELSE  CAST(DATEDIFF(MINUTE,F_DisConnectTime,GETDATE())/1440 as varchar(5)) 
-	                                                    +'天'+CAST( DATEDIFF(MINUTE,F_DisConnectTime,GETDATE())/60 as varchar(5)) 
+	                                                    +'天'+CAST( DATEDIFF(HOUR,F_DisConnectTime,GETDATE())%24 as varchar(5)) 
 	                                                    +'时'+CAST(DATEDIFF(MINUTE,F_DisConnectTime,GETDATE())%60 as varchar(5))+'分' END DiffDate	
                                                     FROM T_ST_MeterUseInfo AS MeterUseInfo
                                                     INNER JOIN T_ST_DataCollectionInfo AS DataCollectionInfo ON DataCollectionInfo.F_CollectionID=MeterUseInfo.F_CollectionID
@@ -35,7 +35,7 @@ namespace EMS.DAL.StaticResources
                                                         ,CASE WHEN  DATEDIFF(MINUTE,DataCollectionInfo.F_LastUpTime,GETDATE()) > 20 OR ( F_DisConnect = 1 OR DATEDIFF(MINUTE,F_DisConnectTime,GETDATE())>10 )THEN '通讯中断' ELSE '通讯正常' END AS States
                                                         ,CASE WHEN F_DisConnectTime IS NULL  OR DATEDIFF(MINUTE,F_DisConnectTime,GETDATE())<15 THEN NULL ELSE F_DisConnectTime END  AS DisConnectTime 
                                                         ,CASE WHEN DATEDIFF(MINUTE,F_DisConnectTime,GETDATE())<15 THEN NULL ELSE  CAST(DATEDIFF(MINUTE,F_DisConnectTime,GETDATE())/1440 as varchar(5)) 
-	                                                        +'天'+CAST( DATEDIFF(MINUTE,F_DisConnectTime,GETDATE())/60 as varchar(5)) 
+	                                                        +'天'+CAST( DATEDIFF(HOUR,F_DisConnectTime,GETDATE())%24 as varchar(5)) 
 	                                                        +'时'+CAST(DATEDIFF(MINUTE,F_DisConnectTime,GETDATE())%60 as varchar(5))+'分' END DiffDate		
                                                     FROM T_ST_MeterUseInfo AS MeterUseInfo
                                                     INNER JOIN T_ST_DataCollectionInfo AS DataCollectionInfo ON DataCollectionInfo.F_CollectionID=MeterUseInfo.F_CollectionID
--- EMS/EMS.UI/Controllers/Circuit/MultiRateController.cs
+++ EMS/EMS.UI/Controllers/Circuit/MultiRateController.cs
@@ -14,6 +14,10 @@ namespace EMS.UI.Controllers.Circuit
     {
         MultiRateService service = new MultiRateService();
 
+        /// <summary>
+        /// 根据用户名，获取建筑列表，第一个建筑能耗分类，第一个建筑的第一个分类的支路列表以及支路的月报表
+        /// </summary>
+        /// <returns></returns>
         public object GetReport()
         {
             string userName = User.Identity.Name;
@@ -21,6 +25,11 @@ namespace EMS.UI.Controllers.Circuit
             return service.GetViewModel(userName);
         }
 
+        /// <summary>
+        /// 根据建筑ID ，获取指定建筑的分类能耗，第一个分类的支路列表以及支路的月报表
+        /// </summary>
+        /// <param name="buildId"></param>
+        /// <returns></returns>
         public object GetReport(string buildId)
         {
             string userName = User.Identity.Name;
@@ -28,6 +37,13 @@ namespace EMS.UI.Controllers.Circuit
             return service.GetViewModel(userName, buildId);
         }
 
+        /// <summary>
+        /// 获取复费率报表
+        /// </summary>
+        /// <param name="buildId">建筑ID</param>
+        /// <param name="type">报表类型（DD-日报；MM-月报；YY-年报）</param>
+        /// <param name="date">日期（yyyy-MM-dd）</param>
+        /// <returns>返回：复费率报表</returns>
         public object GetReport(string buildId, string type, string date)
         {
             return service.GetViewModel(buildId, type, date);
